c4bb05baff74c0e74975ded84c673a6108ec3fdd
rajeshpatankar07 <167472254+rajeshpatankar07@users.noreply.github.com>
2024-07-04 13:24:21 +0000

    container config done

---
+++ js/variable.js
@@ -0,0 +1,9 @@
+const accountId =1415166
+let acccountName = "Rajesh"
+var accountEmail = "rajesh@gmail.com"
+accountCity = "Bhopal"
+let accountPassword = 24222442
+let accuontState;
+
+
+console.table([accountId,acccountName,accountEmail,accountPassword,accountCity,accuontState]);
