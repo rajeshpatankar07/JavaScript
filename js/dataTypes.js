d61e03487e49ef06b50bc57480655ac1e4ad109d
rajeshpatankar07 <167472254+rajeshpatankar07@users.noreply.github.com>
2024-07-09 14:05:03 +0000

    config complete done

---
+++ js/dataTypes.js
@@ -0,0 +1,22 @@
+"user strict"    //threat all js code as a newer version 
+
+//alert(3+3)   //we Are Using Nodejs,not browser
+
+let name = "Rohit"
+let age = 18
+let isLoggedIn = false
+
+// Datatypes in js =>
+//number => 2 to power 53
+//bigint
+//String =>""
+//boolean  =>false
+//null => standAlone value
+// undefined => value is not initialize yet
+//symbol => it is use for uniqeness
+
+//object
+
+console.log(typeof null)//null is object
+console.log(typeof undefined) // datatype is undefined
+console.log(typeof "rohit")  //datatype is String
